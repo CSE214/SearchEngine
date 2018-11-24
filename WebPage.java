26769e0b7b72704c2b2cd94e21e8aafc3cdb7454
Sayan Sivakumaran <sivakusayan@gmail.com>
2018-11-24 03:54:21 -0500

    Initial commit

---
+++ WebPage.java
@@ -0,0 +1,16 @@
+package worldWideWeb;
+
+import java.util.Set;
+
+/**
+ * <code>WebPage</code> class represents a hyperlinked document on the web.
+ *
+ * @author Sayan Sivakumaran e-mail: sayan.sivakumaran@stonybrook.edu Stony
+ *         Brook ID: 110261379
+ **/
+public class WebPage {
+	private String url;
+	private int index;
+	private int rank;
+	private Set<String> keywords;
+}
